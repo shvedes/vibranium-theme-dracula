4b569df6714faee2f40d128c1a88813b1fe18266
shvedes <shved.personal@gmail.com>
2026-05-29 18:12:36 +0200

    Initial commit

---
+++ neovim.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    opts = {
+      transparent_bg = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+}
